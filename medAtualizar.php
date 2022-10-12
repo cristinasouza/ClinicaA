d4f00a1060a21b665be38379ef5d6511e7138ff9
cristina Souza <Cristina.Souza@pucpr.br>
2022-10-12 14:46:43 -0300

    Acerto favicon

--- medAtualizar.php
+++ medAtualizar.php
@@ -9,8 +9,8 @@
 
 <html>
 	<head>
-		<title>IE - Instituição de Ensino</title>
-		<link rel="icon" type="image/png" href="imagens/IE_favicon.png"/>
+		<title>Clínica Médica ABC</title>
+		<link rel="icon" type="image/png" href="imagens/favicon.png"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" href="css/customize.css">
